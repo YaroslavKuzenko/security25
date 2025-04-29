cf5ca2cbed37a1d893304b8878810b3ff97e985e
Yaroslav Kuzenko <yaroslavkuzenko02@gmail.com>
2025-04-30 00:05:13 +0300

    feat: add pre-authorization method interceptor to security configuration

--- src/main/java/edu/kuzenko/security25/config/SecurityConfig.java
+++ src/main/java/edu/kuzenko/security25/config/SecurityConfig.java
@@ -7,8 +7,12 @@ package edu.kuzenko.security25.config;
     @since 24.03.2025 - 22.15
 */
 
+import org.springframework.aop.Advisor;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Role;
+import org.springframework.security.authorization.method.AuthorizationManagerBeforeMethodInterceptor;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,6 +28,12 @@ import org.springframework.http.HttpMethod;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+    public static Advisor preAuthorizeMethodInterceptor() {
+        return AuthorizationManagerBeforeMethodInterceptor.preAuthorize();
+    }
+
     @Bean
     public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
